83ad0deca09105c9080bf161f5c129d7ba38d2ad
Balpreet Saran <balpreet.saran@hotmail.com>
2019-08-25 19:09:04 +1200

    front page updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,32 +19,32 @@
                 <div class="card-body">
                     <form action="/" method="post" encrypt="multipart/form-data">
                         @csrf
-                      <div class="form-group">
-                        <label for="name">Name</label>
-                        <input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}" required>
+                      <div class="form-group row">
+                        <label for="name" class="col-sm-2">Name</label>
+                        <input type="text" class="form-control col-sm-10" name="name" id="name" value="{{ old('name') }}" required>
                         <div class="err">{{ $errors->first('name') }}</div>
                       </div>
-                      <div class="form-group">
-                        <label for="email">Email</label>
-                        <input type="email" class="form-control" name="email" id="email" value="{{ old('email') }}" required>                        
+                      <div class="form-group row">
+                        <label for="email" class="col-sm-2">Email</label>
+                        <input type="email" class="form-control col-sm-10" name="email" id="email" value="{{ old('email') }}" required>                        
                         <div class="err">{{ $errors->first('email') }}</div>
                       </div>
-                      <div class="form-group">
-                        <label for="query">Query Type</label>
-                        <select class="form-control" name="query" id="query" value="{{ old('query') }}" required>
+                      <div class="form-group row">
+                        <label for="query" class="col-sm-2">Query Type</label>
+                        <select class="form-control col-sm-10" name="query" id="query" value="{{ old('query') }}" required>
                           <option>General Query</option>
                           <option>Account Management</option>
                           <option>Technical Support</option>
                         </select>
                       </div>
-                      <div class="form-group">
-                        <label for="subject">Subject</label>
-                        <input type="text" class="form-control" name="subject" id="subject" value="{{ old('subject') }}" required>
+                      <div class="form-group row">
+                        <label for="subject" class="col-sm-2">Subject</label>
+                        <input type="text" class="form-control col-sm-10" name="subject" id="subject" value="{{ old('subject') }}" required>
                         <div class="err">{{ $errors->first('subject') }}</div>
                       </div>
-                      <div class="form-group">
-                        <label for="message">Message</label>
-                        <textarea class="form-control" name="message" id="message" rows="3" required>{{ old('message') }}</textarea>
+                      <div class="form-group row">
+                        <label for="message" class="col-sm-2">Message</label>
+                        <textarea class="form-control col-sm-10" name="message" id="message" rows="3" required>{{ old('message') }}</textarea>
                         <div class="err">{{ $errors->first('message') }}</div>
                       </div>
                       <button type="submit" class="btn btn-primary">Submit</button>
