038558cc2c637a67262d4ae69c5b0b5fa373b3d4
Balpreet Saran <balpreet.saran@hotmail.com>
2019-08-25 17:54:09 +1200

    added html input field validation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,17 +12,17 @@
                         @csrf
                       <div class="form-group">
                         <label for="name">Name</label>
-                        <input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}">
+                        <input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}" required>
                         <div class="err">{{ $errors->first('name') }}</div>
                       </div>
                       <div class="form-group">
                         <label for="email">Email</label>
-                        <input type="email" class="form-control" name="email" id="email" value="{{ old('email') }}">                        
+                        <input type="email" class="form-control" name="email" id="email" value="{{ old('email') }}" required>                        
                         <div class="err">{{ $errors->first('email') }}</div>
                       </div>
                       <div class="form-group">
                         <label for="query">Query Type</label>
-                        <select class="form-control" name="query" id="query" value="{{ old('query') }}">
+                        <select class="form-control" name="query" id="query" value="{{ old('query') }}" required>
                           <option>General Query</option>
                           <option>Account Management</option>
                           <option>Technical Support</option>
@@ -30,12 +30,12 @@
                       </div>
                       <div class="form-group">
                         <label for="subject">Subject</label>
-                        <input type="text" class="form-control" name="subject" id="subject" value="{{ old('subject') }}">
+                        <input type="text" class="form-control" name="subject" id="subject" value="{{ old('subject') }}" required>
                         <div class="err">{{ $errors->first('subject') }}</div>
                       </div>
                       <div class="form-group">
                         <label for="message">Message</label>
-                        <textarea class="form-control" name="message" id="message" rows="3">{{ old('message') }}</textarea>
+                        <textarea class="form-control" name="message" id="message" rows="3" required>{{ old('message') }}</textarea>
                         <div class="err">{{ $errors->first('message') }}</div>
                       </div>
                       <button type="submit" class="btn btn-primary">Submit</button>
