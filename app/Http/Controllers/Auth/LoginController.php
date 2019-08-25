da93483cfbe9a22cd4a168bf5b287ff89bdcb159
Balpreet Saran <balpreet.saran@hotmail.com>
2019-08-25 18:58:38 +1200

    login redirect fix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/submissions';
 
     /**
      * Create a new controller instance.
